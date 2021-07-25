852e69d69f1b27e21e0e86303213f01c3d3fdd8f
Alex <alexspb41@gmail.com>
2021-07-25 13:52:38 +0300

    ADD HW2

---
+++ HW2/HW2/AgentInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class AgentInfo
+    {
+        public int AgentID { get;}
+        public Uri AgentAdress { get;}
+    }
+}
---
+++ HW2/HW2/Controllers/AgentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentsController : ControllerBase
+    {
+        public List<AgentInfo> ListOfAgents { get; set; }
+        public AgentsController()
+        {
+            ListOfAgents = new List<AgentInfo>();
+        }
+
+        [HttpGet("GetListOfAgents")]
+        public IActionResult GetListOfAgents()
+        {
+            return Ok(ListOfAgents);
+        }
+        [HttpPost("register")]
+        public IActionResult RegisterAgent([FromBody] AgentInfo agentInfo)
+        {
+            return Ok();
+        }
+        [HttpPut("enable/{agentID}")]
+        public IActionResult EnableAgentByID([FromRoute] int agentID)
+        {
+            return Ok();
+        }
+        [HttpPut("disable/{agentID}")]
+        public IActionResult DisableAgentByID([FromRoute] int agentID)
+        {
+            return Ok();
+        }
+    }
+}
